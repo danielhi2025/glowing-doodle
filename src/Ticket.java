3c34debc68dfc198560c6ecf06d59b11da0af188
danielhi2025 <admsdaniel7@gmail.com>
2026-02-23 20:29:48 +0200

    Change Ticket class to abstract with price calculation
    Refactored Ticket class to be abstract and added calculatePrice method.

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,4 +1,50 @@
 package PACKAGE_NAME;
 
-public class Ticket {
+public abstract class Ticket {
+    protected final int id;
+    protected final String showName;
+    protected final String showDate;
+    protected final int rowNumber;
+    protected final int seatNumber;
+    protected final Customer ticketCustomer;
+    protected double price;
+    public Ticket(int id, String showName, String showDate,
+                  int rowNumber, int seatNumber,
+                  Customer ticketCustomer, double price) {
+        this.id = id;
+        this.showName = showName;
+        this.showDate = showDate;
+        this.rowNumber = rowNumber;
+        this.seatNumber = seatNumber;
+        this.ticketCustomer = ticketCustomer;
+        this.price = calculatePrice(price);
+    }
+    protected abstract double calculatePrice(double basePrice);
+    public int getId() {
+        return id;
+    }
+
+    public String getShowName() {
+        return showName;
+    }
+
+    public String getShowDate() {
+        return showDate;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public Customer getTicketCustomer() {
+        return ticketCustomer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
