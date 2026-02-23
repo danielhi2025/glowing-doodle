a239e4b93e4f4082dc41627e553de23803b1a0b3
danielhi2025 <admsdaniel7@gmail.com>
2026-02-23 20:32:38 +0200

    Refactor CustomerType enum into Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,59 @@
-public enum CustomerType {
-    REGULAR,
-    VIP
+public class Customer {
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String deliveryAddress;
+    private final CustomerType customerType;
+    private final Double customerDiscount; // Can be null
+    private FoodVoucher foodVoucher; // Not final, can be assigned later
+    public Customer(int id, String firstName, String lastName,
+                    String email, String deliveryAddress,
+                    CustomerType customerType, Double customerDiscount) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.deliveryAddress = deliveryAddress;
+        this.customerType = customerType;
+        this.customerDiscount = customerDiscount;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public Double getCustomerDiscount() {
+        return customerDiscount;
+    }
+    public void takeFoodVoucher(FoodVoucher foodVoucher) {
+        this.foodVoucher = foodVoucher;
+    }
+
+    public void eatFood() {
+        if (foodVoucher != null) {
+            foodVoucher.eat();
+        } else {
+            System.out.println("No food voucher available.");
+        }
+    }
 }
