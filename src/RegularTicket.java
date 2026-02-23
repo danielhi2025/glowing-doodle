45ea6595acced1144ad02882c7133c69e9253fbe
danielhi2025 <admsdaniel7@gmail.com>
2026-02-23 20:27:58 +0200

    Extend RegularTicket class from Ticket

--- src/RegularTicket.java
+++ src/RegularTicket.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
 
-public class RegularTicket {
+public class RegularTicket extends Ticket {
+    public RegularTicket(int id, String showName, String showDate,
+                         int rowNumber, int seatNumber,
+                         Customer ticketCustomer, double price) {
+        super(id, showName, showDate, rowNumber, seatNumber, ticketCustomer, price);
+    }
+    @Override
+    protected double calculatePrice(double basePrice) {
+        return basePrice;
+    }
 }
